e004dc843b2362ded1b8b862999a7a522f9b3ef8
Oskar Fritzner <72510834+oskarfritzner@users.noreply.github.com>
2025-04-08 12:08:18 +0200

    Fix web paths for GitHub Pages deployment

--- Evolvr/webpack.config.js
+++ Evolvr/webpack.config.js
@@ -1,6 +1,20 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: {
+        dangerouslyAddModulePathsToTranspile: ["@gorhom/bottom-sheet"],
+      },
+    },
+    argv
+  );
+
+  // Set the correct public path for GitHub Pages
+  if (env.mode === "production") {
+    config.output.publicPath = "/Evolvr/";
+  }
+
   return config;
 };
